af890f98d7c77e6f66b545c322eb725e186874f7
Saumya Verma <vsaumyav20@gmail.com>
2025-10-05 17:34:55 +0530

    Model added

---
+++ backend/src/main/java/ai_career_roadmap_backend/model/UserInput.java
@@ -0,0 +1,12 @@
+package ai_career_roadmap_backend.model;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class UserInput {
+    private int experience;
+    private String skills;
+    private String targetRole;  
+}
